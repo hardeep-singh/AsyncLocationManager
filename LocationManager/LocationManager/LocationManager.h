5a9bca054d18410a847bc6ab2b765a19eef3f2d0
Hardeep Singh <contact@hardeep-singh.com>
2023-12-23 10:44:54 +1300

    Created new LocationManager framework

---
+++ LocationManager/LocationManager/LocationManager.h
@@ -0,0 +1,18 @@
+//
+//  LocationManager.h
+//  LocationManager
+//
+//  Created by Hardeep Singh on 23/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocationManager.
+FOUNDATION_EXPORT double LocationManagerVersionNumber;
+
+//! Project version string for LocationManager.
+FOUNDATION_EXPORT const unsigned char LocationManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationManager/PublicHeader.h>
+
+
