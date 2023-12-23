caf2a28ff1c59db7fcaa2a0507737fd0fabc6d7a
Hardeep Singh <contact@hardeep-singh.com>
2023-12-23 13:42:28 +1300

    Added the LocationManagerExample project

---
+++ LocationManagerExample/LocationManagerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationManagerExample
+//
+//  Created by Hardeep Singh on 23/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
