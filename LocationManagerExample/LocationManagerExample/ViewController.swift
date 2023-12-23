f01557b314fa8b95cba400f51ce6060fdd8b616f
Hardeep Singh <contact@hardeep-singh.com>
2023-12-23 14:10:54 +1300

    Added the dependency 'AsyncLocationManager' using Pods and imported 'AsyncLocationManager' to utilize the first API for checking location authorization in the LocationManagerExample.

--- LocationManagerExample/LocationManagerExample/ViewController.swift
+++ LocationManagerExample/LocationManagerExample/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import AsyncLocationManager
 
 class ViewController: UIViewController {
 
+    var locationManager = LocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        Task {
+            do {
+                try await locationManager.requestAuthorizationPermission(.always)
+            }
+        }
+    }
 }
 
---
+++ LocationManagerExample/Pods/Target Support Files/AsyncLocationManager/AsyncLocationManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AsyncLocationManager : NSObject
+@end
+@implementation PodsDummy_AsyncLocationManager
+@end
